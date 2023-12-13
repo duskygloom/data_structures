9bc8226bf4b8e9aa81c636a34b3eab93382eda06
duskygloom <sarkarharsh708@gmail.com>
2023-12-13 11:33:05 +0530

    fixed lcm multiple

--- recursive_algorithms/lcm_multiple.c
+++ recursive_algorithms/lcm_multiple.c
@@ -1,12 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int lcm(int a, int b, int multiple) {
-    // multiple is multiple of a
-    if (multiple % b == 0) return multiple;
-    return lcm(a, b, multiple+a);
-}
-
 /**
  * @param a
  * Initialized by array[0].
@@ -14,7 +8,7 @@ int lcm(int a, int b, int multiple) {
  * Initialized by array[1].
  * @param k
  * Initialized by 1.
- * a % kb == 0 is checked.
+ * ka % b == 0 is checked.
  * @param index
  * Initialized by 2.
  * Because array[0] and array[1] are already passed in GCD.
@@ -22,9 +16,10 @@ int lcm(int a, int b, int multiple) {
  * Length will be length of array.
 */
 int lcm_multiple(int a, int b, int k, int *array, int index, int length) {
-    int r = a % (k*b);
-	if (r == 0 && index+1 < length) return lcm_multiple(b, array[index+1], 1, array, index+1, length);
-	else if (r == 0) return (b);
+    int r = (k*a) % b;
+	if (r == 0 && index < length) 
+        return lcm_multiple(k*a, array[index], 1, array, index+1, length);
+	else if (r == 0) return k*a;
     return lcm_multiple(a, b, k+1, array, index, length);
 }
 
